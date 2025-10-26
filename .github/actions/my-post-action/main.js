0506f52cd137e22cc72219ad4bf1ccc5d0433940
Santy <santhumessicr7@gmail.com>
2025-10-26 10:56:56 +0100

    Update action.yml and main.js for clarity; comment out artifact upload/download steps in azure-webapps-node.yml

--- .github/actions/my-post-action/main.js
+++ .github/actions/my-post-action/main.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-
 // Write a value to GITHUB_STATE (so it's available in cleanup.js)
 fs.appendFileSync(process.env.GITHUB_STATE, `name=${process.env.INPUT_NAME}\n`);
-
 console.log(`Hello ${process.env.INPUT_NAME}! (from main.js)`);
